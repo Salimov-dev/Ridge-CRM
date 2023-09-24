6bb28c90ed250b3c8d2be97609fbb16a4d74594e
Salimov-dev <salimov.rent@mail.ru>
2023-09-24 20:49:53 +0300

    новый коммит

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-import { fileURLToPath } from 'url';
-import express from 'express';
-import mongoose from 'mongoose';
-import config from 'config';
-import chalk from 'chalk';
-import cors from 'cors';
-import path from 'path';
-import routes from './routes/index.js';
+import { fileURLToPath } from "url";
+import express from "express";
+import mongoose from "mongoose";
+import config from "config";
+import chalk from "chalk";
+import cors from "cors";
+import path from "path";
+import routes from "./routes/index.js";
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -26,10 +26,11 @@ const PORT = config.get("port") ?? 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client')));
-  const indexPath = path.join(__dirname, 'client', 'index.html');
-  app.get('*', (req, res) => {
+// check
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+  const indexPath = path.join(__dirname, "client", "index.html");
+  app.get("*", (req, res) => {
     res.sendFile(indexPath);
   });
 }
