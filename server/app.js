77c6f662b3248059712a9abf397bdc65fa02a7a4
Salimov-dev <salimov.rent@mail.ru>
2023-09-24 22:15:06 +0300

    сменил apiendpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
-import config from "config";
-import chalk from "chalk";
-import cors from "cors";
-import path from "path";
-import http from "http";
-import routes from "./routes/index.js";
+import { fileURLToPath } from 'url';
+import express from 'express';
+import mongoose from 'mongoose';
+import config from 'config';
+import chalk from 'chalk';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes/index.js';
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -23,14 +23,23 @@ app.use("/api/uploads", express.static("uploads"));
 
 const PORT = config.get("port") ?? 8080;
 
-const server = http.createServer(app);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')));
+  const indexPath = path.join(__dirname, 'client', 'index.html');
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
     await mongoose.connect(config.get("mongoUri"));
     console.log(chalk.green("MongoDB connected"));
 
-    server.listen(PORT, () =>
+    app.listen(PORT, () =>
       console.log(chalk.green(`Server has been started on port ${PORT}`))
     );
   } catch (e) {
