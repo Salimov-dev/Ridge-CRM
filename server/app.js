64ed2078759391bcd92b11aad7ef0adb6a60dc04
Salimov-dev <salimov.rent@mail.ru>
2023-09-26 00:23:34 +0300

    start() is active

--- server/app.js
+++ server/app.js
@@ -28,75 +28,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api", routes);
 app.use("/api/uploads", express.static("uploads"));
 
-// async function start() {
-//   try {
-//     await mongoose.connect(config.get("mongoUri"));
-//     console.log(chalk.green("MongoDB connected"));
-
-//     server.listen(PORT, () =>
-//       console.log(chalk.green(`Server has been started on port ${PORT}`))
-//     );
-//   } catch (e) {
-//     console.log(chalk.red(e.message));
-//     process.exit(1);
-//   }
-// }
-
-// start();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
-// const corsOptions = {
-//   allowedHeaders: [
-//     "content-type",
-//     "Access-Control-Allow-Origin",
-//     "Access-Control-Allow-Credentials",
-//     "Origin",
-//     "X-Requested-With",
-//     "Authorization",
-//     "Accept",
-//   ],
-//   origin: [
-//     "https://www.ridge-crm.ru",
-//     "https://dev-craft-kappa.vercel.app",
-//     "http://localhost:5173",
-//   ],
-//   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-//   optionSuccessStatus: 200,
-//   credentials: true,
-// };
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.green("MongoDB connected"));
 
-// const app = express();
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}`))
+    );
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
 
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+start();
 
-// app.use("/api", routes);
-// app.use("/api/uploads", express.static("uploads"));
-
-// const PORT = config.get("port") || 5000;
-
-// const server = http.createServer(app);
-
-// async function start() {
-//   try {
-//     await mongoose.connect(config.get("mongoUri"));
-//     console.log(chalk.green("MongoDB connected"));
-
-//     server.listen(PORT, () =>
-//       console.log(chalk.green(`Server has been started on port ${PORT}`))
-//     );
-//   } catch (e) {
-//     console.log(chalk.red(e.message));
-//     process.exit(1);
-//   }
-// }
-
-// start();
