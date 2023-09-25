53a28491737f31e8c8d2fd6a325415ab1647a185
Salimov-dev <salimov.rent@mail.ru>
2023-09-26 01:07:48 +0300

    изменил mongoURI

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.use("/api/uploads", express.static("uploads"));
 async function start() {
   try {
     await mongoose.connect(
-      "mongodb+srv://ruspb1987:rtkNpn2w1Jc8poKQ@cluster0.ejnptrn.mongodb.net/?retryWrites=true&w=majority/test"
+      "mongodb://ruspb1987:rtkNpn2w1Jc8poKQ@ac-1hnuvn3-shard-00-00.ejnptrn.mongodb.net:27017,ac-1hnuvn3-shard-00-01.ejnptrn.mongodb.net:27017,ac-1hnuvn3-shard-00-02.ejnptrn.mongodb.net:27017/?ssl=true&replicaSet=atlas-ty0rfj-shard-0&authSource=admin&retryWrites=true&w=majority/test"
     );
 
     app.listen(PORT, () => {
