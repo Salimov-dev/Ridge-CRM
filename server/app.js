05024f2eafc3e03757ebfcc36d522f12203e54cc
Salimov-dev <salimov.rent@mail.ru>
2023-09-25 21:59:59 +0300

    app.options в app.js

--- server/app.js
+++ server/app.js
@@ -29,6 +29,12 @@ const corsOptions = {
 const app = express();
 
 app.use(cors(corsOptions));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.status(200).send();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
