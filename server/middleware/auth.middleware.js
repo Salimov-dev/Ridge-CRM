70a9c973079be7b3055b2976c1693050ce8bedeb
Salimov-dev <salimov.rent@mail.ru>
2023-09-27 01:39:13 +0300

    auth.middleware logs

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -7,14 +7,18 @@ export default (req, res, next) => {
 
   try {
     const token = req.headers.authorization.split(" ")[1];
-
+    console.log("token", token);
+    
     if (!token) {
       return res.status(401).json({ message: "Не авторизован" });
     }
+    console.log("!token");
 
     const data = tokenService.validateAccess(token);
-
+    console.log("data", data);
+    
     req.user = data;
+    console.log("req.user", req.user);
 
     next();
   } catch (e) {
