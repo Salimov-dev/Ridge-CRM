1e2671d309ae69cea5597a7f2de685f8047b05b9
Salimov-dev <salimov.rent@mail.ru>
2023-09-27 22:40:39 +0300

    new vercel 3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,26 +9,29 @@ import { YMaps } from "@pbe/react-yandex-maps";
 import { BrowserRouter } from "react-router-dom";
 // providers
 import { Provider } from "react-redux";
+import React from "react";
 
 const store = createStore();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <YMaps
-      query={{
-        apikey: "fe7c4f02-9876-404c-91b2-c6816e373307",
-      }}
-      version={"2.1"}
-    >
-      <BrowserRouter>
-        <App />
-        <Box style={{ width: "10px" }}>
-          <ScrollUpButton
-            ContainerClassName="AnyClassForContainer"
-            TransitionClassName="AnyClassForTransition"
-          ></ScrollUpButton>
-        </Box>
-      </BrowserRouter>
-    </YMaps>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <YMaps
+        query={{
+          apikey: "fe7c4f02-9876-404c-91b2-c6816e373307",
+        }}
+        version={"2.1"}
+      >
+        <BrowserRouter>
+          <App />
+          <Box style={{ width: "10px" }}>
+            <ScrollUpButton
+              ContainerClassName="AnyClassForContainer"
+              TransitionClassName="AnyClassForTransition"
+            ></ScrollUpButton>
+          </Box>
+        </BrowserRouter>
+      </YMaps>
+    </Provider>
+  </React.StrictMode>
 );
