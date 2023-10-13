30fc9e9e5b64f4c43b56e9e0acf9cb4d2526f831
Salimov-dev <salimov.rent@mail.ru>
2023-10-13 18:55:48 +0300

    api 31.129 etc

--- server/app.js
+++ server/app.js
@@ -17,7 +17,6 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:3000",
     "http://31.129.108.151",
-    "https://ridge-crm.ru",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: [
